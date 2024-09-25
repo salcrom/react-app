997043a46ba0500436cde0487a6605350d779dee
salcrom <seya.28@gmail.com>
2024-09-25 18:57:52 +0200

    Control Props Implementado

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,7 +1,7 @@
 import { createContext, CSSProperties, ReactElement } from 'react';
 
 import { useProduct } from '../hooks/useProduct'
-import { ProductContextProps, Product } from '../interfaces/interfaces';
+import { ProductContextProps, Product, onChangeArgs } from '../interfaces/interfaces';
 
 import styles from '../styles/styles.module.css'
 
@@ -15,13 +15,15 @@ export interface Props {
     children?: ReactElement | ReactElement[];
     className?: string;
     style?: CSSProperties;
+    onChange?: ( args: onChangeArgs ) => void;
+    value?: number;
 }
 
 
 
-export const ProductCard = ({ children, product, className, style }: Props ) => {
+export const ProductCard = ({ children, product, className, style, onChange, value }: Props ) => {
     
-    const { counter, increaseBy } = useProduct()
+    const { counter, increaseBy } = useProduct({ onChange, product, value })
 
     return (
         <Provider value={{
--- src/02-component-patterns/interfaces/interfaces.ts
+++ src/02-component-patterns/interfaces/interfaces.ts
@@ -16,8 +16,17 @@ export interface ProductContextProps {
 }
 
 export interface ProductCardHOCProps {
-    ({ children, product }: ProductCardProps):JSX.Element,
-    Buttons: ( Props: ProductButtonsProps ) => JSX.Element
-    Image: ( Props: ProductImageProps ) => JSX.Element,
-    Title: ( Props: ProductTitleProps ) => JSX.Element,
+    ({ children, product }: ProductCardProps):JSX.Element;
+    Buttons: ( Props: ProductButtonsProps ) => JSX.Element;
+    Image: ( Props: ProductImageProps ) => JSX.Element;
+    Title: ( Props: ProductTitleProps ) => JSX.Element;
+}
+
+export interface onChangeArgs {
+    product: Product;
+    count: number;
+}
+
+export interface ProductInCart extends Product {
+    count: number;
 }
